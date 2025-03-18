8390e934fae3cc637c40447af2e08983a7b77ef1
MariuszSolvd <mkaminski@solvd.com>
2025-03-18 14:54:14 +0100

    Add Second Test(correct login)

--- src/test/java/com/solvd/TestApp.java
+++ src/test/java/com/solvd/TestApp.java
@@ -1,7 +1,9 @@
 package com.solvd;
 
 import com.solvd.pages.common.LoginPage;
+import com.solvd.pages.common.ProductPage;
 import com.zebrunner.carina.core.AbstractTest;
+import com.zebrunner.carina.utils.R;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -13,5 +15,12 @@ public class TestApp extends AbstractTest {
         LoginPage loginPage = initPage(getDriver(), LoginPage.class);
         loginPage.login("standard", "wrong");
         assertTrue(loginPage.isErrorVisible());
+    }
+
+    @Test
+    public void shouldLogin() {
+        LoginPage loginPage = initPage(getDriver(), LoginPage.class);
+        ProductPage productPage = loginPage.login(R.TESTDATA.get("user.standard"), R.TESTDATA.get("user.password"));
+        assertTrue(productPage.isPageOpened());
     }
 }
