7a2e2cefc34295edf8f8d9f19206c62c56245ae4
MariuszSolvd <mkaminski@solvd.com>
2025-03-18 14:22:06 +0100

    Implemented first Test

--- src/test/java/com/solvd/TestApp.java
+++ src/test/java/com/solvd/TestApp.java
@@ -4,11 +4,14 @@ import com.solvd.pages.common.LoginPage;
 import com.zebrunner.carina.core.AbstractTest;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.*;
+
 public class TestApp extends AbstractTest {
 
     @Test
-    public void testing() {
+    public void shouldNotLogin() {
         LoginPage loginPage = initPage(getDriver(), LoginPage.class);
         loginPage.login("standard", "wrong");
+        assertTrue(loginPage.isErrorVisible());
     }
 }
