7479d74ea01fcdde13f3b536470611c8a9162060
MariuszSolvd <mkaminski@solvd.com>
2025-03-19 14:15:22 +0100

    Added MenuPage

---
+++ src/main/java/com/solvd/pages/common/MenuPage.java
@@ -0,0 +1,15 @@
+package com.solvd.pages.common;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public abstract class MenuPage extends AbstractPage {
+
+    public MenuPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public abstract void clickButton(int buttonNumber);
+
+    public abstract LoginPage clickLogout();
+}
---
+++ src/main/java/com/solvd/pages/ios/MenuPageIOS.java
@@ -0,0 +1,35 @@
+package com.solvd.pages.ios;
+
+import com.solvd.pages.common.LoginPage;
+import com.solvd.pages.common.MenuPage;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+@DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = MenuPage.class)
+public class MenuPageIOS extends MenuPage {
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[`name BEGINSWITH \"ALL\"`][4]/*")
+    private List<ExtendedWebElement> menuButtons;
+
+    public MenuPageIOS(WebDriver driver) {
+        super(driver);
+        setUiLoadedMarker(menuButtons.getFirst());
+    }
+
+    //Contains 9 buttons
+    @Override
+    public void clickButton(int buttonNumber) {
+        menuButtons.get(buttonNumber).click();
+    }
+
+    @Override
+    public LoginPage clickLogout() {
+        menuButtons.get(7).click();
+        return initPage(getDriver(), LoginPage.class);
+    }
+
+}
