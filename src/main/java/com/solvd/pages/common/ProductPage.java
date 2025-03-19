d9f3ca644304a7190e2fc744cab032908f3160c3
MariuszSolvd <mkaminski@solvd.com>
2025-03-19 13:23:58 +0100

    Added Header to ProductPage

--- src/main/java/com/solvd/pages/common/ProductPage.java
+++ src/main/java/com/solvd/pages/common/ProductPage.java
@@ -1,9 +1,8 @@
 package com.solvd.pages.common;
 
-import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
 
-public abstract class ProductPage extends AbstractPage {
+public abstract class ProductPage extends HeaderPage {
 
     public ProductPage(WebDriver driver) {
         super(driver);
--- src/main/java/com/solvd/pages/ios/ProductPageIOS.java
+++ src/main/java/com/solvd/pages/ios/ProductPageIOS.java
@@ -1,6 +1,8 @@
 package com.solvd.pages.ios;
 
 import com.solvd.pages.common.ProductPage;
+import com.solvd.pages.common.elements.Header;
+import com.solvd.pages.ios.elements.HeaderIOS;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
@@ -9,11 +11,18 @@ import org.openqa.selenium.WebDriver;
 @DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = ProductPage.class)
 public class ProductPageIOS extends ProductPage {
 
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[$name == \"headerContainer\"$][-2]")
+    private HeaderIOS headerIOS;
     @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeStaticText[`name == \"PRODUCTS\"`]")
     private ExtendedWebElement productText;
 
     public ProductPageIOS(WebDriver driver) {
         super(driver);
         setUiLoadedMarker(productText);
+    }
+
+    @Override
+    public Header getHeader() {
+        return headerIOS;
     }
 }
