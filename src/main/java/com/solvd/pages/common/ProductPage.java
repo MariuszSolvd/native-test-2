1da86665c2aa47415114bb3549174ac8760af614
MariuszSolvd <mkaminski@solvd.com>
2025-03-19 17:32:07 +0100

    Added Product to ProductPage

--- src/main/java/com/solvd/pages/common/ProductPage.java
+++ src/main/java/com/solvd/pages/common/ProductPage.java
@@ -1,10 +1,15 @@
 package com.solvd.pages.common;
 
+import com.solvd.pages.common.elements.Product;
 import org.openqa.selenium.WebDriver;
+
+import java.util.List;
 
 public abstract class ProductPage extends HeaderPage {
 
     public ProductPage(WebDriver driver) {
         super(driver);
     }
+
+    public abstract List<? extends Product> getProducts();
 }
--- src/main/java/com/solvd/pages/ios/ProductPageIOS.java
+++ src/main/java/com/solvd/pages/ios/ProductPageIOS.java
@@ -2,11 +2,15 @@ package com.solvd.pages.ios;
 
 import com.solvd.pages.common.ProductPage;
 import com.solvd.pages.common.elements.Header;
+import com.solvd.pages.common.elements.Product;
 import com.solvd.pages.ios.elements.HeaderIOS;
+import com.solvd.pages.ios.elements.ProductIOS;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
+
+import java.util.List;
 
 @DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = ProductPage.class)
 public class ProductPageIOS extends ProductPage {
@@ -15,6 +19,8 @@ public class ProductPageIOS extends ProductPage {
     private HeaderIOS headerIOS;
     @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeStaticText[`name == \"PRODUCTS\"`]")
     private ExtendedWebElement productText;
+    @ExtendedFindBy(iosPredicate = "name == \"test-Item\"")
+    private List<ProductIOS> products;
 
     public ProductPageIOS(WebDriver driver) {
         super(driver);
@@ -22,7 +28,14 @@ public class ProductPageIOS extends ProductPage {
     }
 
     @Override
+    public List<? extends Product> getProducts() {
+        return products;
+    }
+
+    @Override
     public Header getHeader() {
         return headerIOS;
     }
+
+
 }
