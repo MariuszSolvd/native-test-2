a416e250fa810c411f59fd500524b0699de84817
MariuszSolvd <mkaminski@solvd.com>
2025-03-20 13:33:42 +0100

    Removed click button methods

--- src/main/java/com/solvd/pages/common/elements/Footer.java
+++ src/main/java/com/solvd/pages/common/elements/Footer.java
@@ -1,5 +1,6 @@
 package com.solvd.pages.common.elements;
 
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
@@ -11,11 +12,5 @@ public abstract class Footer extends AbstractUIObject {
         super(driver, searchContext);
     }
 
-    public abstract void clickTwitterButton();
-
-    public abstract void clickFBButton();
-
-    public abstract void clickGoogleButton();
-
-    public abstract void clickLinkedinButton();
+    public abstract ExtendedWebElement getTwitterLogo();
 }
