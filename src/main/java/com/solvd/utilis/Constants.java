2e7c40984d189bbfe5468ec62dd37accf81834c2
MariuszSolvd <mkaminski@solvd.com>
2025-03-20 12:22:40 +0100

    Added Constants

---
+++ src/main/java/com/solvd/utilis/Constants.java
@@ -0,0 +1,7 @@
+package com.solvd.utilis;
+
+public final class Constants {
+
+    public static final String HEADER_IOS = "**/XCUIElementTypeOther[$name == \"headerContainer\"$][-2]";
+    public static final String FOOTER_IOS = "name == \"\uF099 \uF09A \uF0D5 \uF0E1\"";
+}
