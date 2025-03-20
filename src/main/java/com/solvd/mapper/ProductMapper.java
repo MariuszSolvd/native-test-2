cd841c490e8e8c35030f1dda042d98851b770f8b
MariuszSolvd <mkaminski@solvd.com>
2025-03-20 17:44:07 +0100

    Swipe is now controlled by Product

--- src/main/java/com/solvd/mapper/ProductMapper.java
+++ src/main/java/com/solvd/mapper/ProductMapper.java
@@ -14,7 +14,6 @@ public final class ProductMapper {
     }
 
     public static ProductEnt mapToProductEnt (Product product) {
-        product.swipeToProduct();
         return new ProductEnt(product.getNameText(), product.getPrice());
     }
 }
