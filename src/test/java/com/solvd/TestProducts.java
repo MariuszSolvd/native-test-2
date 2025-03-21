6d97b2935833e48c5ce10447ca046f0f73c9f2fa
MariuszSolvd <mkaminski@solvd.com>
2025-03-21 12:58:26 +0100

    Improved 1st test

--- src/test/java/com/solvd/TestProducts.java
+++ src/test/java/com/solvd/TestProducts.java
@@ -19,16 +19,14 @@ public class TestProducts extends AbstractTest {
     @Test
     public void sortProductsAtoZ() {
         ProductPage productPage = LoginService.login();
-        List<? extends Product> products = productPage.getProducts();
-        List<ProductEnt> sortedProducts = ProductMapper.mapAllToProductEnt(products)
+        List<ProductEnt> sortedProducts = ProductMapper.mapAllToProductEnt(productPage.getProducts())
                 .stream()
                 .sorted(Comparator.comparing(ProductEnt::name))
                 .toList();
         SelectionPage selectionPage = productPage.clickSelectionButton();
         productPage = selectionPage.sortAtoZButton();
         productPage.backToFirstProduct();
-        products = productPage.getProducts();
-        List<ProductEnt> afterSortList = ProductMapper.mapAllToProductEnt(products);
+        List<ProductEnt> afterSortList = ProductMapper.mapAllToProductEnt(productPage.getProducts());
         assertEquals(sortedProducts, afterSortList);
     }
 
