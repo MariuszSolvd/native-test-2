2305ffcf65c5d23d0f7f6eef4ca834786099c5a7
MariuszSolvd <mkaminski@solvd.com>
2025-03-21 11:57:35 +0100

    Implemented first Test for ProductPage

--- src/test/java/com/solvd/TestProducts.java
+++ src/test/java/com/solvd/TestProducts.java
@@ -3,24 +3,33 @@ package com.solvd;
 import com.solvd.enitiy.ProductEnt;
 import com.solvd.mapper.ProductMapper;
 import com.solvd.pages.common.ProductPage;
+import com.solvd.pages.common.SelectionPage;
 import com.solvd.pages.common.elements.Product;
 import com.solvd.service.LoginService;
 import com.zebrunner.carina.core.AbstractTest;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
-import static org.testng.Assert.*;
-
+import static org.testng.Assert.assertEquals;
 
 public class TestProducts extends AbstractTest {
 
     @Test
-    public void sortLowestPrice() {
+    public void sortProductsAtoZ() {
         ProductPage productPage = LoginService.login();
-        //TODO: Lazy loading of products needed!!
-        List<ProductEnt> presortProducts = ProductMapper.mapAllToProductEnt(productPage.getProducts());
-        presortProducts.forEach(System.out::println);
+        List<? extends Product> products = productPage.getProducts();
+        List<ProductEnt> sortedProducts = ProductMapper.mapAllToProductEnt(products)
+                .stream()
+                .sorted(Comparator.comparing(ProductEnt::name))
+                .toList();
+        SelectionPage selectionPage = productPage.clickSelectionButton();
+        productPage = selectionPage.sortAtoZButton();
+        productPage.backToFirstProduct();
+        products = productPage.getProducts();
+        List<ProductEnt> afterSortList = ProductMapper.mapAllToProductEnt(products);
+        assertEquals(sortedProducts, afterSortList);
     }
 
 }
