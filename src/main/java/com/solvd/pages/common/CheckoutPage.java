6cfa3610418d7e7054ec509e25abdcd48aa37963
MariuszSolvd <mkaminski@solvd.com>
2025-03-25 14:47:12 +0100

    Add CheckoutPage

---
+++ src/main/java/com/solvd/pages/common/CheckoutPage.java
@@ -0,0 +1,10 @@
+package com.solvd.pages.common;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class CheckoutPage extends CommonPage {
+
+    public CheckoutPage(WebDriver driver) {
+        super(driver);
+    }
+}
