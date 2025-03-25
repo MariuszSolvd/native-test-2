83d87f15ecf63729685e25349148eb01ce69d835
MariuszSolvd <mkaminski@solvd.com>
2025-03-25 14:46:55 +0100

    Change in implementation

--- src/main/java/com/solvd/pages/common/CartPage.java
+++ src/main/java/com/solvd/pages/common/CartPage.java
@@ -1,10 +1,25 @@
 package com.solvd.pages.common;
 
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
 
 public abstract class CartPage extends  CommonPage{
 
     public CartPage(WebDriver driver) {
         super(driver);
+    }
+
+    public abstract ExtendedWebElement getShoppingButton();
+
+    public abstract ExtendedWebElement getCheckoutButton();
+
+    public ProductPage backToProductPage() {
+        getShoppingButton().click();
+        return initPage(getDriver(), ProductPage.class);
+    }
+
+    public CheckoutPage goToCheckoutPage() {
+        getCheckoutButton();
+        return initPage(getDriver(), CheckoutPage.class);
     }
 }
--- src/main/java/com/solvd/pages/ios/CartPageIOS.java
+++ src/main/java/com/solvd/pages/ios/CartPageIOS.java
@@ -1,12 +1,14 @@
 package com.solvd.pages.ios;
 
 import com.solvd.pages.common.CartPage;
+import com.solvd.pages.common.ProductPage;
 import com.solvd.pages.common.elements.Footer;
 import com.solvd.pages.common.elements.Header;
 import com.solvd.pages.ios.elements.FooterIOS;
 import com.solvd.pages.ios.elements.HeaderIOS;
 import com.solvd.utilis.Constants;
 import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 
@@ -17,10 +19,26 @@ public class CartPageIOS extends CartPage {
     private HeaderIOS headerIOS;
     @ExtendedFindBy(iosPredicate = Constants.FOOTER_IOS)
     private FooterIOS footerIOS;
+    @ExtendedFindBy(iosPredicate = "name == \"test-CONTINUE SHOPPING\"")
+    private ExtendedWebElement shopButton;
+    @ExtendedFindBy(iosPredicate = "name == \"test-CHECKOUT\"")
+    private ExtendedWebElement checkoutButton;
+
 
     public CartPageIOS(WebDriver driver) {
         super(driver);
     }
+
+    @Override
+    public ExtendedWebElement getShoppingButton() {
+        return shopButton;
+    }
+
+    @Override
+    public ExtendedWebElement getCheckoutButton() {
+        return checkoutButton;
+    }
+
 
     @Override
     public Header getHeader() {
